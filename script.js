ead74f51dbacadcf829819f92b9cdb5851450163
Methlor <keelersamuel@gmail.com>
2020-10-24 18:55:22 +0300

    Pausing until Monday, Saved the URL and Timestamp as a comment in JS file.

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-// 17.36 Timer
+// Going to Pause project until Monday. Looking into Unity Projects, Link to Tutorial - https://youtu.be/Y-GkMjUZsmM?list=PLWElJP47damxD6xO_nokajkfyMj-rNXSR&t=1056 
+// 17.36 
